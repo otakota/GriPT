b1aaf0288005c8b6cb893c0f510062f890e27364
otakota <otakota268@gmail.com>
2025-03-16 16:54:13 +0900

    add login func

---
+++ src/app/scraping.ts
@@ -0,0 +1,41 @@
+import puppeteer from "puppeteer";
+
+const id =process.env.ID;
+const PassWord=process.env.PASSWORD;
+console.log("id",id);
+console.log("PassWord",PassWord);
+    if( id === undefined || PassWord === undefined){
+        throw new Error("ID or PassWord is not")
+}
+
+
+
+const login = async (
+    ID:string,
+    PassWord:string,
+
+) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.rakuten-sec.co.jp/ITS/V_ACT_Login.html",{waitUntil : "domcontentloaded"});
+  await page.type('#form-login-id',ID);
+  await page.type('#form-login-pass',PassWord);
+  await page.click('#login-btn');
+  try{
+    await page.waitForNavigation({waitUntil:"domcontentloaded",timeout:5000})
+  }
+  catch(e){
+    console.log("ログインできませんでした")
+  }
+  const amount =await page.evaluate(()=>document.querySelector('#asset_total_amount')?.innerHTML)
+  console.log(amount,typeof amount)
+
+
+  //const amount = await page.evaluate(()=> document.querySelector('.asset-total-amount')?.innerHTML);
+  //console.log(amount)
+
+  await browser.close()
+}
+
+login(id,PassWord)
+
